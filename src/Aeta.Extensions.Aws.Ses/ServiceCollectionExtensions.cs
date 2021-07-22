a2c9216811903d5303f08bbceb381b060f14d08f
Arinze Anozie <arinze.anozie@aetainvestments.com>
2021-07-22 14:08:32 -0400

    Add IServiceCollection extension for Amazon SES

---
+++ src/Aeta.Extensions.Aws.Ses/ServiceCollectionExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Amazon.SimpleEmailV2;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Aeta.Extensions.Aws.Ses
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddAmazonSimpleEmailService(this IServiceCollection services,
+            ServiceLifetime lifetime = ServiceLifetime.Scoped,
+            Action<AmazonSimpleEmailServiceV2Config> configModifications = null)
+        {
+            var sesClient = new ServiceDescriptor(typeof(IAmazonSimpleEmailServiceV2), serviceProvider =>
+            {
+                var config = serviceProvider.GetAmazonSesClientConfig();
+                configModifications?.Invoke(config);
+
+                return new AmazonSimpleEmailServiceV2Client(config);
+            }, lifetime);
+
+            services.Add(sesClient);
+            return services;
+        }
+
+        private static AmazonSimpleEmailServiceV2Config GetAmazonSesClientConfig(this IServiceProvider serviceProvider)
+        {
+            var configuration = serviceProvider.GetService<IConfiguration>()?
+                .GetSection("amazon")?
+                .GetSection("ses");
+
+            var amazonConfig = new AmazonSimpleEmailServiceV2Config();
+            if (configuration is null) return amazonConfig;
+
+            configuration.Bind(amazonConfig);
+            return amazonConfig;
+        }
+    }
+}
